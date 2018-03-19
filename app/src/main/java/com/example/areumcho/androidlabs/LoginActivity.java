8d245f3c3e55770a0085ee62a9a9517f9ea56497
areumcho <cho00052@algonquinlive.com>
2018-03-19 03:42:12 -0400

    Added 3 activies for Lab 2

---
+++ app/src/main/java/com/example/areumcho/androidlabs/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.areumcho.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
